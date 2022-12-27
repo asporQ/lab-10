27cd36d952eda4ebaad982bda0c852a3c176c1e5
asporQ <tikump.aspor@gmail.com>
2022-12-27 17:11:37 +0700

    complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,24 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//Write the function printO() here
+// Write the function printO() here
+void printO(int N, int M)
+{
+	int i = 0, j = 0;
+	if (N <= 0 || M <= 0)
+	{
+		cout << "Invalid input";
+	}
+	while (j < N)
+	{
+		i = 0;
+		while (i < M)
+		{
+			cout << "O";
+			i++;
+		}
 
-int main(){
-	
-	printO(2,2);
+		cout << "\n";
+		j++;
+	}
+}
+
+int main()
+{
+
+	printO(2, 2);
 	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
+
+	printO(3, 5);
+	cout << "\n";
+
+	printO(5, 3);
+	cout << "\n";
+
+	printO(0, 3);
+	cout << "\n";
+
+	printO(7, -1);
+	cout << "\n";
+
 	return 0;
 }
