4dd41ea937f38e8c12514d4997412b8779b8dfb0
asporQ <tikump.aspor@gmail.com>
2022-12-27 17:34:24 +0700

    complete

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -12,11 +12,6 @@ int main()
 	cout << "Enter amount you can pay per year: ";
 	cin >> payment;
 
-	int year = 1;
-	double interest, newBalance = loan, total;
-	interest = loan * (rate / 100);
-	total = newBalance + interest;
-	newBalance = total - payment;
 
 	cout << setw(13) << left << "EndOfYear#";
 	cout << setw(13) << left << "PrevBalance";
@@ -27,29 +22,37 @@ int main()
 	cout << "\n";
 
 	cout << fixed << setprecision(2);
-	while (loan > 0)
+
+	int year = 1;
+	double preBalance=loan,interest, newBalance, total;
+	interest = loan * (rate / 100);
+	total = preBalance + interest;
+	newBalance = total - payment;
+
+	while (newBalance > 0 && payment < loan)
 	{
-		if(loan<=payment){
-			payment = total;
-			newBalance = 0;
-		}
+
 		cout << setw(13) << left << year;
-		cout << setw(13) << left << loan;
+		cout << setw(13) << left << preBalance;
 		cout << setw(13) << left << interest;
 		cout << setw(13) << left << total;
 		cout << setw(13) << left << payment;
 		cout << setw(13) << left << newBalance;
 		cout << "\n";
-		loan = newBalance;
-		interest = newBalance * (rate / 100);
-		total = newBalance + interest;
-
-		year++;
-		if (newBalance < payment)
-		{
-			payment = total;
-		}
+		preBalance = newBalance;
+		interest = preBalance * (rate / 100);
+		total = preBalance + interest;
 		newBalance = total - payment;
+		year++;
 	}
+		cout << setw(13) << left << year;
+		cout << setw(13) << left << preBalance;
+		cout << setw(13) << left << interest;
+		cout << setw(13) << left << total;
+		payment = total;
+		cout << setw(13) << left << payment;
+		newBalance=total-payment;
+		cout << setw(13) << left << newBalance;
+
 	return 0;
 }
